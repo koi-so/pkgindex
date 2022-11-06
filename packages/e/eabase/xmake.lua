87a72d3f72db8af59617acb5f965c6d24a55647b
socean <lumengeared@gmail.com>
2022-11-06 14:37:49 +0800

    removed dpp, added EA libraries

---
+++ packages/e/eabase/xmake.lua
@@ -0,0 +1,23 @@
+package("eabase")
+    set_kind("library", { headeronly = true })
+    set_homepage("https://github.com/koi-so/EABase")
+    set_description(
+        "EABase is a small set of header files that define platform-independent data types and platform feature macros."
+    )
+    set_license("BSD-3-Clause")
+
+    add_urls("https://github.com/koi-so/EABase/archive/$(version).tar.gz")
+    add_versions("2.09.10", "521cb053d9320636f53226ffc616216cf532f0ef")
+
+    on_install("windows", "linux", "macosx", function(package)
+        os.cp("include/Common/EABase", package:installdir("include"))
+    end)
+
+    on_test(function(package)
+        assert(
+            package:has_cxxfuncs(
+                "EA_LIMITS_DIGITS_U(int)",
+                { configs = { languages = "c++17" }, includes = "EABase/eabase.h" }
+            )
+        )
+    end)
---
+++ packages/e/eastl/xmake.lua
@@ -0,0 +1,32 @@
+package("eastl")
+    set_homepage("https://github.com/koi-so/EASTL/")
+    set_description("EASTL stands for Electronic Arts Standard Template Library.")
+    set_license("BSD-3-Clause")
+
+    add_urls("https://github.com/koi-so/EASTL/archive/$(version).tar.gz")
+    add_versions("3.18.01", "19a88692bed1797cd98e97df862e8ccfff0f0489")
+
+    -- add_deps("cmake")
+    add_deps("eabase")
+
+    on_install("windows", "linux", "macosx", function(package)
+        io.replace("CMakeLists.txt", "add_subdirectory(test/packages/EABase)", "", { plain = true })
+        io.replace("CMakeLists.txt", "target_link_libraries(EASTL EABase)", "", { plain = true })
+        local configs = { "-DEASTL_BUILD_TESTS=OFF", "-DEASTL_BUILD_BENCHMARK=OFF" }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        if not package:is_plat("windows") then
+            table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        end
+        import("package.tools.cmake").install(package, configs, { packagedeps = "eabase" })
+        os.cp("include/EASTL", package:installdir("include"))
+    end)
+
+    on_test(function(package)
+        assert(package:check_cxxsnippets({
+            test = [[
+                void test() {
+                    eastl::vector<int> testInt{};
+                }
+            ]],
+        }, { configs = { languages = "c++17" }, includes = "EASTL/vector.h" }))
+    end)
